6ba3b612d726ffe32c2fd19f43d03f7b2aa29f72
huixing <1216740282qq.com>
2018-01-23 21:28:16 +0800

    第2次提交

---
+++ app/src/main/java/com/example/huixing/decprectice/Login/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.example.huixing.decprectice.Login;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import com.example.huixing.decprectice.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
